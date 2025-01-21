5f20d63a97a14712ab292e70fa42b0f8daf9f8cb
tusharbhayani <tushar.bhayani@ayanworks.com>
2025-01-21 19:25:01 +0530

    fix: remove history ui from home screen and library
    Signed-off-by: tusharbhayani <tushar.bhayani@ayanworks.com>

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -3,21 +3,18 @@ import React, { ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Dimensions, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-import HistoryListItem from '../components/History/HistoryListItem'
-import { getGenericRecordsByQuery } from '../components/History/HistoryManager'
-import { CustomRecord, RecordType } from '../components/History/types'
 import ScanButton from '../components/common/ScanButton'
+import NotificationListItem, { NotificationType } from '../components/listItems/NotificationListItem'
+import NoNewUpdates from '../components/misc/NoNewUpdates'
+import { AttachTourStep } from '../components/tour/AttachTourStep'
 import { useConfiguration } from '../contexts/configuration'
-import { useStore } from '../contexts/store'
 import { useTheme } from '../contexts/theme'
 import { HomeStackParams, Screens } from '../types/navigators'
 
-import ListCredentials from './ListCredentials'
-
-interface EnhancedW3CRecord extends W3cCredentialRecord {
-  connectionLabel?: string
-}
+const { width } = Dimensions.get('window')
 const offset = 25
+const offsetPadding = 5
+
 type HomeProps = StackScreenProps<HomeStackParams, Screens.Home>
 
 const Home: React.FC<HomeProps> = ({ navigation }) => {
@@ -25,9 +22,6 @@ const Home: React.FC<HomeProps> = ({ navigation }) => {
   const { notifications } = useCustomNotifications()
   const { t } = useTranslation()
   const { HomeTheme } = useTheme()
-  const [store] = useStore()
-  const [historyItems, setHistoryItems] = useState<CustomRecord[]>()
-  const { credentialListOptions: CredentialListOptions } = useConfiguration()
 
   const styles = StyleSheet.create({
     container: {
@@ -68,167 +62,86 @@ const Home: React.FC<HomeProps> = ({ navigation }) => {
       height: '85%',
       width: '80%',
     },
-    cardView: { flexDirection: 'row', justifyContent: 'center', marginVertical: 20, width: wp('100%') },
-    cardContentView: {
-      backgroundColor: ColorPallet.brand.primary,
-      width: wp('45%'),
-      height: 80,
-      marginHorizontal: 5,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 10,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-      elevation: 3,
-      alignContent: 'center',
-    },
-    countCardText: {
-      ...HomeTheme.notificationsHeader,
-      color: '#fff',
-    },
-    cardText: {
-      color: '#fff',
-      fontSize: 12,
-      fontWeight: '900',
-      fontStyle: 'normal',
-    },
-    viewText: {
-      ...HomeTheme.notificationsHeader,
-      fontSize: 14,
-      fontWeight: '500',
-      fontStyle: 'normal',
-      alignSelf: 'flex-end',
-      marginRight: 20,
-    },
-    historyText: {
-      ...HomeTheme.welcomeHeader,
-      fontSize: 16,
-      fontWeight: 'bold',
-      fontStyle: 'normal',
-      marginLeft: 20,
-    },
-    cardBottomBorder: {
-      borderBottomWidth: 0.5,
-      borderBottomColor: '#A0A4AB',
-      marginTop: 10,
-    },
-    headerView: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignContent: 'center',
-      alignItems: 'center',
-      marginVertical: 10,
-    },
-    title: {
-      marginTop: 16,
-    },
-    credentialsCardList: { flexGrow: 0, marginLeft: 15 },
-    renderView: {
-      marginRight: 15,
-      marginTop: 15,
-      width: wp('85%'),
-    },
-    favContainer: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-      minHeight: 170,
-      marginLeft: 10,
-    },
   })
 
-  const renderEmptyListComponent = () => {
-    return (
-      <View style={{ alignContent: 'center', justifyContent: 'center', alignSelf: 'center' }}>
-        <Text style={[styles.title, TextTheme.normal]}>{t('ActivityHistory.NoHistory')}</Text>
-      </View>
-    )
+  const DisplayListItemType = (item: any): ReactNode => {
+    let component: ReactNode = <View style={{ backgroundColor: 'red', height: 30 }} />
+    if (item.type === 'CredentialRecord') {
+      let notificationType = NotificationType.CredentialOffer
+      if (item.revocationNotification) {
+        notificationType = NotificationType.Revocation
+      }
+      component = <NotificationListItem notificationType={notificationType} notification={item} />
+    } else if (item.type === 'CustomNotification') {
+      component = <NotificationListItem notificationType={NotificationType.Custom} notification={item} />
+    } else {
+      component = <NotificationListItem notificationType={NotificationType.ProofRequest} notification={item} />
+    }
+    return component
   }
 
-  const renderHistoryListItem = (item: CustomRecord) => {
-    return <HistoryListItem item={item} />
-  }
-  const renderHistoryView = () => {
-    return (
-      <View>
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          style={{ flexGrow: 0, paddingHorizontal: 20 }}
-          data={historyItems}
-          ListEmptyComponent={renderEmptyListComponent}
-          renderItem={element => renderHistoryListItem(element.item)}
-        />
-      </View>
-    )
-  }
   return (
     <View style={styles.container}>
-      <CredentialListOptions />
-      <View style={styles.cardView}>
-        <TouchableOpacity
-          style={styles.cardContentView}
-          onPress={() => {
-            navigation.navigate(Stacks.ContactStack, { screen: Screens.Contacts, params: { navigation: navigation } })
-          }}>
-          <Text style={styles.countCardText}>{`${connectionCount}`}</Text>
-          <Text style={styles.cardText}>{t('Home.ConnectionsCount')}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.cardContentView}
-          onPress={() => {
-            navigation.navigate(TabStacks.CredentialStack, { screen: Screens.Credentials })
-          }}>
-          <Text style={styles.countCardText}>{`${credentialCount}`}</Text>
-          <Text style={styles.cardText}>{t('Home.CredentialsCount')}</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* {credentialList && credentialList?.length >0 && */}
-      <View style={styles.headerView}>
-        <Text style={styles.historyText}>{t('Global.Credentials')}</Text>
-
-        {credentialList && credentialList?.length > 0 && (
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate(TabStacks.CredentialStack, { screen: Screens.Credentials })
-            }}>
-            <Text style={styles.viewText}>{t('Home.ViewAll')}</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-
-      <View style={styles.favContainer}>
-        <ListCredentials isHorizontal />
-      </View>
-
-      <View>
-        <View style={styles.headerView}>
-          <Text style={styles.historyText}>{t('Global.History')}</Text>
-          {historyItems && historyItems.length > 0 && (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate(Stacks.HistoryStack, { screen: Screens.HistoryPage })
-              }}>
-              <Text style={styles.viewText}>{t('Home.ViewAll')}</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-      </View>
-
-      <ScrollView>
-        {historyItems && historyItems.length > 0 && store.preferences.useHistoryCapability ? (
-          renderHistoryView()
+      <View style={styles.rowContainer}>
+        {notifications?.length > 0 ? (
+          <AttachTourStep index={1} fill>
+            <Text style={[HomeTheme.notificationsHeader, styles.header]}>
+              {t('Home.Notifications')}
+              {notifications?.length ? ` (${notifications.length})` : ''}
+            </Text>
+          </AttachTourStep>
         ) : (
-          <View style={styles.messageContainer}>
-            <Image source={require('../assets/img/homeimage.png')} resizeMode="contain" style={styles.homeImage} />
+          <Text style={[HomeTheme.notificationsHeader, styles.header]}>
+            {t('Home.Notifications')}
+            {notifications?.length ? ` (${notifications.length})` : ''}
+          </Text>
+        )}
+        {notifications?.length > 1 ? (
+          <TouchableOpacity
+            style={styles.linkContainer}
+            activeOpacity={1}
+            onPress={() => navigation.navigate(Screens.Notifications)}>
+            <Text style={styles.link}>{t('Home.SeeAll')}</Text>
+          </TouchableOpacity>
+        ) : null}
+      </View>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        scrollEnabled={notifications?.length > 0 ? true : false}
+        style={{ flexGrow: 0 }}
+        snapToOffsets={[
+          0,
+          ...Array(notifications?.length)
+            .fill(0)
+            .map((n: number, i: number) => i * (width - 2 * (offset - offsetPadding)))
+            .slice(1),
+        ]}
+        decelerationRate="fast"
+        ListEmptyComponent={() => (
+          <View style={{ marginHorizontal: offset, width: width - 2 * offset }}>
+            <View>
+              <NoNewUpdates />
+            </View>
+          </View>
+        )}
+        data={notifications}
+        keyExtractor={item => item.id}
+        renderItem={({ item, index }) => (
+          <View
+            style={{
+              width: width - 2 * offset,
+              marginLeft: !index ? offset : offsetPadding,
+              marginRight: index === notifications?.length - 1 ? offset : offsetPadding,
+            }}>
+            {DisplayListItemType(item)}
           </View>
         )}
       />
       <View style={styles.messageContainer}>
         <Image source={require('../assets/img/homeimage.png')} resizeMode="contain" style={styles.homeImage} />
       </View>
+      {/* <Button title="Create did" onPress={createDid} /> */}
       <View style={styles.fabContainer}>
         <ScanButton />
       </View>
